2aa8eb14ec9fa64197c34609ff0eb50bb62cfe1e
DenisKrajacic <deniskrajacic9@gmail.com>
2021-12-20 01:32:10 +0100

    Program

--- Aplikacija/Form1.Designer.cs
+++ Aplikacija/Form1.Designer.cs
@@ -28,13 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.boxId = new System.Windows.Forms.TextBox();
+            this.boxNaziv = new System.Windows.Forms.TextBox();
+            this.boxTrajanje = new System.Windows.Forms.TextBox();
+            this.boxZanr = new System.Windows.Forms.TextBox();
+            this.boxOrig = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnBegin = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnIzmeni = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.boxZemlja = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // boxId
+            // 
+            this.boxId.Enabled = false;
+            this.boxId.Location = new System.Drawing.Point(113, 45);
+            this.boxId.Name = "boxId";
+            this.boxId.Size = new System.Drawing.Size(262, 20);
+            this.boxId.TabIndex = 0;
+            // 
+            // boxNaziv
+            // 
+            this.boxNaziv.Location = new System.Drawing.Point(113, 71);
+            this.boxNaziv.Name = "boxNaziv";
+            this.boxNaziv.Size = new System.Drawing.Size(262, 20);
+            this.boxNaziv.TabIndex = 1;
+            // 
+            // boxTrajanje
+            // 
+            this.boxTrajanje.Location = new System.Drawing.Point(113, 97);
+            this.boxTrajanje.Name = "boxTrajanje";
+            this.boxTrajanje.Size = new System.Drawing.Size(262, 20);
+            this.boxTrajanje.TabIndex = 2;
+            // 
+            // boxZanr
+            // 
+            this.boxZanr.Location = new System.Drawing.Point(113, 123);
+            this.boxZanr.Name = "boxZanr";
+            this.boxZanr.Size = new System.Drawing.Size(262, 20);
+            this.boxZanr.TabIndex = 3;
+            // 
+            // boxOrig
+            // 
+            this.boxOrig.Location = new System.Drawing.Point(113, 149);
+            this.boxOrig.Name = "boxOrig";
+            this.boxOrig.Size = new System.Drawing.Size(262, 20);
+            this.boxOrig.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Naziv :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Trajanje :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Žanr :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Naziv originala :";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(219, 241);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 12;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(138, 241);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 13;
+            this.btnPrev.Text = "<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(300, 241);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 14;
+            this.btnEnd.Text = ">>";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Location = new System.Drawing.Point(57, 241);
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(75, 23);
+            this.btnBegin.TabIndex = 15;
+            this.btnBegin.Text = "<<";
+            this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(99, 270);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(75, 23);
+            this.btnDodaj.TabIndex = 16;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // btnIzmeni
+            // 
+            this.btnIzmeni.Location = new System.Drawing.Point(180, 270);
+            this.btnIzmeni.Name = "btnIzmeni";
+            this.btnIzmeni.Size = new System.Drawing.Size(75, 23);
+            this.btnIzmeni.TabIndex = 17;
+            this.btnIzmeni.Text = "Izmeni";
+            this.btnIzmeni.UseVisualStyleBackColor = true;
+            this.btnIzmeni.Click += new System.EventHandler(this.btnIzmeni_Click);
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(261, 270);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(75, 23);
+            this.btnObrisi.TabIndex = 18;
+            this.btnObrisi.Text = "Obriši";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            this.btnObrisi.Click += new System.EventHandler(this.btnObrisi_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Zemlja :";
+            // 
+            // boxZemlja
+            // 
+            this.boxZemlja.Location = new System.Drawing.Point(113, 175);
+            this.boxZemlja.Name = "boxZemlja";
+            this.boxZemlja.Size = new System.Drawing.Size(262, 20);
+            this.boxZemlja.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "F I L M O T O P I J A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(426, 323);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnIzmeni);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnBegin);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boxZemlja);
+            this.Controls.Add(this.boxOrig);
+            this.Controls.Add(this.boxZanr);
+            this.Controls.Add(this.boxTrajanje);
+            this.Controls.Add(this.boxNaziv);
+            this.Controls.Add(this.boxId);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(442, 362);
+            this.MinimumSize = new System.Drawing.Size(442, 362);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox boxId;
+        private System.Windows.Forms.TextBox boxNaziv;
+        private System.Windows.Forms.TextBox boxTrajanje;
+        private System.Windows.Forms.TextBox boxZanr;
+        private System.Windows.Forms.TextBox boxOrig;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnBegin;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnIzmeni;
+        private System.Windows.Forms.Button btnObrisi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox boxZemlja;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Aplikacija/Form1.cs
+++ Aplikacija/Form1.cs
@@ -7,14 +7,152 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Aplikacija
 {
     public partial class Form1 : Form
     {
+        public int Red = 0;
+        DataTable film = new DataTable();
+        string cs = "Data source=.; Initial catalog=Forma; Integrated security=true";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection veza = new SqlConnection(cs);
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT ID, naziv, trajanje, zanr, naziv_originala, zemlja FROM Filmovi ORDER BY ID", veza);
+            adapter.Fill(film);
+            FillBoxes(Red);
+        }
+
+        private void FillBoxes(int id)
+        {
+            if (film.Rows.Count == 0)
+            {
+                boxId.Text = "";
+                boxNaziv.Text = "";
+                boxTrajanje.Text = "";
+                boxZanr.Text = "";
+                boxOrig.Text = "";
+                boxZemlja.Text = "";
+
+                btnNext.Enabled = false;
+                btnEnd.Enabled = false;
+                btnPrev.Enabled = false;
+                btnBegin.Enabled = false;
+            }
+            else
+            {
+                boxId.Text = film.Rows[Red]["ID"].ToString();
+                boxNaziv.Text = film.Rows[Red]["naziv"].ToString();
+                boxTrajanje.Text = film.Rows[Red]["trajanje"].ToString();
+                boxZanr.Text = film.Rows[Red]["zanr"].ToString();
+                boxOrig.Text = film.Rows[Red]["naziv_originala"].ToString();
+                boxZemlja.Text = film.Rows[Red]["zemlja"].ToString();
+
+                btnNext.Enabled = (Red != film.Rows.Count - 1);
+                btnEnd.Enabled = (Red != film.Rows.Count - 1);
+
+                btnPrev.Enabled = (Red != 0);
+                btnBegin.Enabled = (Red != 0);
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            Red++;
+            FillBoxes(Red);
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            Red--;
+            FillBoxes(Red);
+        }
+
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            Red = 0;
+            FillBoxes(Red);
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Red = film.Rows.Count - 1;
+            FillBoxes(Red);
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            string komanda = "INSERT INTO Filmovi (naziv, trajanje, zanr, naziv_originala, zemlja) VALUES ('"
+                + boxNaziv.Text
+                + "', '" + boxTrajanje.Text
+                + "', '" + boxZanr.Text
+                + "', '" + boxOrig.Text
+                + "', '" + boxZemlja.Text + "')";
+
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand dodaj = new SqlCommand(komanda, veza);
+
+            veza.Open();
+            dodaj.ExecuteNonQuery();
+            veza.Close();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT ID, naziv, trajanje, zanr, naziv_originala, zemlja FROM Filmovi ORDER BY ID", veza);
+            film.Clear();
+            adapter.Fill(film);
+            Red = film.Rows.Count - 1;
+            FillBoxes(Red);
+        }
+
+        private void btnObrisi_Click(object sender, EventArgs e)
+        {
+            string komanda = "DELETE FROM Filmovi WHERE ID = " + boxId.Text;
+
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand dodaj = new SqlCommand(komanda, veza);
+
+            veza.Open();
+            dodaj.ExecuteNonQuery();
+            veza.Close();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT ID, naziv, trajanje, zanr, naziv_originala, zemlja FROM Filmovi ORDER BY ID", veza);
+            film.Clear();
+            adapter.Fill(film);
+            if (Red > film.Rows.Count - 1)
+            {
+                Red = film.Rows.Count - 1;
+            }
+            FillBoxes(Red);
+        }
+
+        private void btnIzmeni_Click(object sender, EventArgs e)
+        {
+            string komanda = "UPDATE Filmovi SET naziv = '"
+                + boxNaziv.Text
+                + "', trajanje = '" + boxTrajanje.Text
+                + "', zanr = '" + boxZanr.Text
+                + "', naziv_originala = '" + boxOrig.Text
+                + "', zemlja = '" + boxZemlja.Text
+                + "' WHERE id = " + boxId.Text;
+
+
+            SqlConnection veza = new SqlConnection(cs);
+            SqlCommand dodaj = new SqlCommand(komanda, veza);
+
+            veza.Open();
+            dodaj.ExecuteNonQuery();
+            veza.Close();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT ID, naziv, trajanje, zanr, naziv_originala, zemlja FROM Filmovi ORDER BY ID", veza);
+            film.Clear();
+            adapter.Fill(film);
+            FillBoxes(Red);
         }
     }
 }
